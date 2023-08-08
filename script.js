122bce45564eebf309f12589a1b0b71a227ea558
NisovaKi <oksy169516@gmail.com>
2023-08-08 19:52:27 +0300

    Обновлен script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 "use strict";
 
-
 const productName = "apple"; // наименование товара
 const sum = 10; // количество
-const category = "products" // категория товара
+const categoryProducts = "products" // категория товара
 const price = 60; // цена
 
 /* ✔ В консоль разработчика выведите наименование товара */
@@ -14,4 +13,5 @@ console.log('Наименование товара:', productName);
 
 console.log('Общая стоимость товара:', sum * price);
 
-
+const item = "кондиционер Sony"
+const categoryTechnic = "technic"
