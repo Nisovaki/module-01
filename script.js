ce724c4dbe0002089bc13c95bbb4081d9277d46f
NisovaKi <oksy169516@gmail.com>
2023-08-09 13:14:57 +0300

    Добавлены фигурные скобки

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+{
 const productName = "apple"; // наименование товара
 const sum = 10; // количество
 const categoryProducts = "products" // категория товара
@@ -12,6 +12,9 @@ console.log('Наименование товара:', productName);
 /* ✔ В консоль выведите общую сумму товара */
 
 console.log('Общая стоимость товара:', sum * price);
+}
 
+{
 const item = "кондиционер Sony"
 const categoryTechnic = "technic"
+}
