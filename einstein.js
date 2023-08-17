805c4bf4fc8be91ab08743be08e2d363b1cde05a
NisovaKi <oksy169516@gmail.com>
2023-08-17 17:26:55 +0300

    homework changed

--- einstein.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-{
-  const myWeight = 7; // вес кота)))
-  const speedLight = 3e8;
-  const energy = myWeight * speedLight * speedLight
-  console.log(energy);
-}
--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 "use strict";
-
 { 
   const productMod = prompt('Product name');
   const numMod = +prompt('Quantity of goods');
   const categoryMod = prompt('Product category');
   const priceMod = +prompt('Product price');
 
-  console.log(`Product name: ${productMod} type: ${typeof productMod}`);
-  // console.log(`Quantity of goods: ${numMod} type: ${typeof numMod}`);
-  console.log(`Product category: ${categoryMod} type: ${typeof categoryMod}`);
-  console.log(`Product price: ${priceMod} type: ${typeof priceMod}`);
-
-  console.log(`In stock ${numMod} product units, ${productMod} for the amount of ${numMod * priceMod} wooden`);
-
-  if (numMod) {
+  if (productMod && numMod && priceMod && categoryMod) {
+    console.log(`Product name: ${productMod} type: ${typeof productMod}`);
     console.log(`Quantity of goods: ${numMod} type: ${typeof numMod}`);
+    console.log(`Product category: ${categoryMod} type: ${typeof categoryMod}`);
+    console.log(`Product price: ${priceMod} type: ${typeof priceMod}`);
   } else {
     console.log('You have entered incorrect data');
   };
